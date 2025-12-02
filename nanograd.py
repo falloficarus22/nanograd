5ba5797e9ee4f83bde568c9eaaf17dfe2653973f
Abhishek <norizzabhii@gmail.com>
2025-12-02 10:48:36 +0000

    Initial commit

---
+++ nanograd.py
@@ -0,0 +1,58 @@
+import math
+
+class Value:
+    def __init__(self, data, chidren = (), _op = ''):
+        self.data = data
+        self.grad = 0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.grad * out.grad
+            other.grad += self.grad * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def tanh(self):
+        t = (math.exp(2 * self.data) - 1) / (math.exp(2 * self.data) + 1)
+        out = Value(t, (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward():
+        topo = []
+        visited = set()
+
+        def build(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build(child)
+                topo.append(v)
+
+        build(self)
+        self.grad = 1.0
+
+        for node in reversed(topo):
+            node._backward()
